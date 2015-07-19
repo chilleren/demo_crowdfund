259ae5e5a931d6cf0e5db5ab14acb25d89665a2e
Chris Hilleren <chilleren@gmail.com>
2015-07-18 18:12:41 -0700

    add resourceful routes for campaigns and users, and root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :campaigns
+  resources :users, only: [:new, :create, :edit, :update, :show]
+
+  root 'campaigns#index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
