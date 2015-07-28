b1681db8ed668a4379387ae4dfe7175f5464120a
Chris Hilleren <chilleren@gmail.com>
2015-07-27 21:20:35 -0700

    update user route, validations. session validation.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def new
     @user = User.new
@@ -25,6 +27,14 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update user_params
+      flash[:notice] = "Account updated successfully."
+      redirect_to edit_user_path @user
+    else
+      flash[:alert] = @user.errors.full_messages.join(", ")
+      redirect_to edit_user_path @user
+    end
   end
 
   private
@@ -32,5 +42,16 @@ class UsersController < ApplicationController
       params.require(:user).permit(:email, :password, :password_confirmation)
     end
 
+    def logged_in_user
+      unless logged_in?
+        flash[:alert] = "Please log in first"
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      raise ActionController::RoutingError.new('Not Found') unless current_user == @user
+    end
 
 end
